167f98febeccf1ab0885304ef9ae8b941be9d5b6
stefen menzel <stefen.menzel@biworldwide.com>
2025-04-28 11:46:50 -0500

    feat: nvim initial config

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {
+      view = {
+        width = 45,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    }
+  end,
+}
